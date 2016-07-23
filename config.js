452d416bbdc880f6274dae152854ac62aef82c28
Markov Sergii <mr_sv@mail.ru>
2016-07-23 17:59:57 +0300

    add module files adn file db config

--- config.js
+++ config.js
@@ -4,3 +4,20 @@
 "use strict";
 
 module.exports.secret = 'some_sekret!';
+
+
+module.exports.db_config = {
+    db : '',
+    user : '',
+    password : '',
+    mysql : {
+        host: '93.171.158.114',
+        dialect: 'mysql',
+        collate: "utf8_general_ci",
+        pool: {
+            max: 10,
+            min: 0,
+            idle: 10000
+        }
+    }
+};
---
+++ models/models.js
@@ -0,0 +1,30 @@
+/**
+ * Created by user on 23.07.2016.
+ */
+'use strict';
+
+let Sequelize = require('sequelize');
+let db_config = require('../config').db_config;
+
+let sequelize = new Sequelize(db_config.db , db_config.user, db_config.password, db_config.mysql);
+
+let obj = {
+    sequelize: sequelize,
+    Users: sequelize.define('users', {
+
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    })
+
+
+};
+
+//obj.Users.sync({force: true});
+
+module.exports = obj;
